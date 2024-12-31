6e6e75c105b672e448d780713bc74a8d6958bdd0
阿福 <569558422@qq.com>
2024-12-31 18:39:14 +0800

    更新Hex

--- Assets/Scripts/Hexago/Hexago.cs
+++ Assets/Scripts/Hexago/Hexago.cs
@@ -56,8 +56,6 @@ namespace Assets.Scripts.Hexago
 
         private Orientation _orientation;
 
-        private Vector3[] _hexPoints = new Vector3[6];
-
         public float HexSize = 1f;
 
         public int2 StartPoint = int2.zero;
@@ -166,17 +164,13 @@ namespace Assets.Scripts.Hexago
         {
             Gizmos.color = Color.black;
 
-            for (int i = 0; i < 10; i++)
+            for (int i = -10; i < 10; i++)
             {
-                for (int j = 0; j < 10; j++)
+                for (int j = -10; j < 10; j++)
                 {
-                    var pointX = StartPoint.x + i * Mathf.Sqrt(3) * HexSize;
-                    if (j % 2 == 0)
-                    {
-                        pointX = Mathf.Sqrt(3) / 2* HexSize + pointX;
-                    }
-                    var point = new Vector3(pointX, 0, j * (3f / 2f * HexSize)+StartPoint.y);
-                    DrawHex(point);
+                    var hex = new Hex(i, -i - j, j);
+                    Gizmos.color = Color.black;
+                    DrawHexList(PolygonCorners(hex));
                 }
             }
 
@@ -221,20 +215,5 @@ namespace Assets.Scripts.Hexago
         }
 
 
-        private void DrawHex(Vector3 center)
-        {
-         
-            for (int i = 0; i < 6; i++)
-            {
-                var angle_deg = 60 * i - 30;
-                var angle_rad = Mathf.PI / 180f * angle_deg;
-                _hexPoints[i] = new Vector3(center.x + HexSize * Mathf.Cos(angle_rad), 0, center.z + HexSize * Mathf.Sin(angle_rad));
-            }
-            for (int i = 0; i < _hexPoints.Length; i++)
-            {
-                var index = (i + 1) % (_hexPoints.Length);
-                Gizmos.DrawLine(_hexPoints[i], _hexPoints[index]);
-            }
-        }
     }
 }
